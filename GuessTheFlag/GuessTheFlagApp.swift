5b5c0fdec3fe69fb67ccc621121447c72a44f2b4
Rafael Fagundes <rafaelcflima@gmail.com>
2024-08-28 12:12:43 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Rafael Fagundes on 2024-08-28.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
