683c9491566148bfcdc3a28db377aaa92ef4717c
Rafael Fagundes <rafaelcflima@gmail.com>
2024-08-30 00:39:30 -0400

    Initial commit

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,117 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let numberOfFlags: Int = 3
+
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var isShowingAnswer: Bool = false
+    @State private var scoreTitle = "Wrong"
+    @State private var score = 0
+    @State private var totalClicks = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [Color(red: 0.03, green: 0.96, blue: 0.62), Color(red: 0.26, green: 0.02, blue: 0.49)], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+
+            VStack(spacing: 30) {
+                Spacer()
+                logoView
+                scoreView
+                promptView
+
+                ForEach(0..<numberOfFlags, id: \.self) { number in
+                    flagButton(for: number)
+                }
+            }
+
+            if isShowingAnswer {
+                answerOverlay
+            }
         }
-        .padding()
+    }
+    
+    private var scoreView: some View {
+        Text("Score: \(score)/\(totalClicks)")
+            .padding()
+            .background(Color.white)
+            .clipShape(Capsule())
+    }
+    
+    private var logoView: some View {
+        Image("gtf-logo")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding(.horizontal, 30)
+    }
+    
+    private var promptView: some View {
+        VStack {
+            Text("Tap the flag of")
+                .foregroundStyle(.white)
+                .font(.subheadline.weight(.heavy))
+            Text(countries[correctAnswer])
+                .foregroundStyle(.white)
+                .font(.largeTitle.weight(.semibold))
+        }
+    }
+    
+    private func flagButton(for number: Int) -> some View {
+        Button {
+            flagTapped(number)
+        } label: {
+            Image(countries[number])
+                .resizable()
+                .scaledToFit()
+                .shadow(radius: 25)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+    }
+    
+    private var answerOverlay: some View {
+        Color.primary
+            .background(.thinMaterial)
+            .opacity(0.75)
+            .ignoresSafeArea()
+            .overlay(
+                HStack {
+                    Image(systemName: scoreTitle == "Correct" ? "checkmark.circle" : "x.circle")
+                        .font(.system(size: 50))
+                        .foregroundColor(.white)
+                    
+                    Text(scoreTitle)
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .padding()
+                .background(scoreTitle == "Correct" ? Color.green : Color.red)
+                .clipShape(Capsule())
+                .shadow(radius: 10)
+            )
+    }
+    
+    private func flagTapped(_ number: Int) {
+        totalClicks += 1
+        if number == correctAnswer {
+            score += 1
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        isShowingAnswer = true
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            askQuestion()
+            isShowingAnswer = false
+        }
+    }
+    
+    private func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0..<numberOfFlags)
     }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-#  <#Title#>
+# Guess The Flag
 
+This app is part of the 100 Days of Swift course by Paul Hudson.
+
+You can follow the tutorial for this app here:
+https://www.hackingwithswift.com/books/ios-swiftui/guess-the-flag-introduction
+
+## Demo
+
+![Demo](app.gif)
+
+## My app contains some changes that I found interesting and fun to make:
+
+- Instead of an alert that requires an extra tap and isn't very game-like, I made an overlay that darkens the background and shows the result for 1 second, then automatically disappears and continues the game, making it more dynamic.
+- I used glyphs from SF Symbols in the overlay message.
+- I created a very simple logo to improve the overall look of the app.
+- I created a capsule with the user's score, showing the number of correct answers versus attempts.
+- In the source code, I separated the views into different variables to make the code more readable and modular (even though everything is still in the same source code for simplicity's sake).
+- I learned to use a form of timeout in Swift, similar to `setTimeout` in JavaScript.
+
+## Future improvements:
+
+- Include all the flags of the world.
+- Add animations to make the game more dynamic.
+
+## Conclusion
+
+This app was quite interesting to expand my knowledge in SwiftUI, considering that my previous experience in college was using Storyboards.
